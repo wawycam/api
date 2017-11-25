6824abca907765165022e9f73ed1abec9f587c1e
Stéphane legouffe <slegouffe@gmail.com>
2017-11-25 17:03:11 +0100

    Rewrite "WiFi" Apis Clean old routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,5 @@ module.exports = function(server) {
   require('./service')(server);
   require('./video')(server);
   require('./wawy')(server);
-
-  require('./version')(server);
-  require('./live')(server);
-  require('./snap')(server);
-  require('./settings')(server);
+  require('./wifi')(server);
 };
