2e1aff26294573471bd0271a413caa08f409a373
Stephane LEGOUFFE <stephane@emotinobox.camera>
2017-11-27 10:57:07 +0000

    Add missing files

---
+++ index.js
@@ -0,0 +1,36 @@
+const restify = require('restify');
+const restifyPlugins = require('restify-plugins');
+const corsMiddleware = require('restify-cors-middleware')
+const mongoose = require('mongoose');
+
+const config = require('./config');
+const WaWy = require('./controllers/wawy');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/wawy');
+
+const cors = corsMiddleware({
+  preflightMaxAge: 5,
+  origins: ['*'],
+  allowHeaders: ['GET', 'POST'],
+})
+
+const server = restify.createServer({
+  name: config.name,
+  version: config.version,
+  acceptable: ['application/json', 'image/png']
+});
+
+WaWy.init();
+
+server.pre(cors.preflight);
+server.use(cors.actual);
+server.use(restifyPlugins.bodyParser({ mapParams: true }));
+server.use(restifyPlugins.acceptParser(server.acceptable));
+server.use(restifyPlugins.queryParser({ mapParams: true }));
+server.use(restifyPlugins.fullResponse());
+
+server.listen(config.port, function () {
+  require('./routes')(server);
+  console.log(`Server ${server.name} is listening on port ${config.port}`);
+}); 
