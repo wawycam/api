26e4ee0b79eb3c42f5d531c2a58d2c4618ea9dea
Stéphane legouffe <slegouffe@gmail.com>
2017-11-27 11:56:23 +0100

    Add "Picam" directory Change Picam exec path in controller

--- controllers/picam.js
+++ controllers/picam.js
@@ -8,7 +8,7 @@ module.exports = {
   start: (callback) => {
     Settings.get((settings) => {
       console.log(`Start picam with ${settings.camera.rotation}° rotation lens`);
-      picam = exec(`/home/pi/picam/picam --noaudio --fps 30 -v 2000000 --rotation ${settings.camera.rotation} -w 1280 -h 720 -o /run/shm/hls`);
+      picam = exec(`./picam/picam --noaudio --fps 30 -v 2000000 --rotation ${settings.camera.rotation} -w 1280 -h 720 -o /run/shm/hls`);
       Settings.set({isBroadcasting: true});
       picam.stdout.on('data', (data) => {
         console.log(data.trim());
