fb0b3c215a63159d88706b0714562e5f90cee300
Stephane LEGOUFFE <stephane@emotinobox.camera>
2018-06-15 15:49:42 +0000

    Send WaWy Serial on RTS Connection

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const restify = require('restify');
 const restifyPlugins = require('restify-plugins');
 const corsMiddleware = require('restify-cors-middleware')
-const socketio = require('socket.io');
 const mongoose = require('mongoose');
 const Logger = require('./utils/logger');
 const config = require('./config');
 const WaWy = require('./controllers/wawy');
-const RTS = require('../RTS-Client/lib')('http://192.168.0.70', '1234');
+const RTS = require('../RTS-Client/lib')();
 
 const cors = corsMiddleware({
   preflightMaxAge: 5,
@@ -28,16 +27,6 @@ server.use(restifyPlugins.queryParser({ mapParams: true }));
 server.use(restifyPlugins.fullResponse());
 server.use(RTS.listen);
 
-const io = socketio(server.server);
-
-io.on('connection', function(socket) {
-  console.log('Someone is connnecting !');
-  socket.emit('msg', { msg: 'world' });
-  socket.on('my other event', function(data) {
-      console.log(data);
-  });
-});
-
 module.exports = server.listen(config.port, function () {
   
   mongoose.Promise = global.Promise;
@@ -51,6 +40,7 @@ module.exports = server.listen(config.port, function () {
 
   db.once('open', () => {
     WaWy.init((wawy) => {
+      RTS.connect(wawy.serial);
       RTS.set(wawy.serial);
       require('./routes')(server, wawy, RTS);
       Logger.info(`Server ${server.name} is listening on port ${config.port}`);
